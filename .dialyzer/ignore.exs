5eb1688bf76af7aef6937536b7aefad51c8081bc
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2021-09-29 16:48:57 +0200

    Numbered lists (dialyzer)

--- .dialyzer/ignore.exs
+++ .dialyzer/ignore.exs
@@ -1,5 +1,5 @@
 [
   {"lib/md/parser.ex", :pattern_match, 0},
-  {"lib/md/parser.ex", :call, 393},
-  {"lib/md/parser.ex", :pattern_match_cov, 494}
+  {"lib/md/parser.ex", :call, 405},
+  {"lib/md/parser.ex", :pattern_match_cov, 506}
 ]
