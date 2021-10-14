d79ad97311dc84945a95fb31e719dadcea2fde6a
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2021-10-14 09:05:01 +0200

    Benchmarks

---
+++ bench/engines_bench.exs
@@ -0,0 +1,13 @@
+defmodule Md.Bench do
+  use Benchfella
+
+  @input File.read!("priv/earmark.md")
+
+  bench "earmark" do
+    Earmark.as_html!(@input)
+  end
+
+  bench "md" do
+    Md.generate(@input)
+  end
+end
